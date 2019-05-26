86640b89999aa5c582e6d45b359a0ef2f1c97051
Kyle Tress <kyle@kyletress.com>
2019-05-25 21:18:53 -0400

    initialize git repo, move to after_bundle

--- template.rb
+++ template.rb
@@ -1,3 +1,12 @@
-# Create and migrate the database
-rails_command "db:create"
-rails_command "db:migrate"
+after_bundle do
+    # Create and migrate the database
+    rails_command "db:create"
+    rails_command "db:migrate"
+
+    # Initialize a git repo and commit everything 
+    git :init 
+    git add: "."
+    git commit: %Q{ -m 'Initial commit' }
+
+    say "Track is clear. Your app has been created.", :green
+end
