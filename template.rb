9721bebc3813185dd999c0abe7077e2363117ad4
Kyle Tress <kyle@kyletress.com>
2019-05-26 01:19:54 -0400

    Force SSL in production

--- template.rb
+++ template.rb
@@ -7,65 +7,68 @@ require "shellwords"
 # invoked remotely via HTTP, that means the files are not present locally.
 # In that case, use `git clone` to download them to a local temporary dir.
 def add_template_repository_to_source_path
-    if __FILE__ =~ %r{\Ahttps?://}
-      require "tmpdir"
-      source_paths.unshift(tempdir = Dir.mktmpdir("groove-"))
-      at_exit { FileUtils.remove_entry(tempdir) }
-      git clone: [
-        "--quiet",
-        "https://github.com/kyletress/groove.git",
-        tempdir
-      ].map(&:shellescape).join(" ")
-  
-      if (branch = __FILE__[%r{groove/(.+)/template.rb}, 1])
-        Dir.chdir(tempdir) { git checkout: branch }
-      end
-    else
-      source_paths.unshift(File.dirname(__FILE__))
+  if __FILE__ =~ %r{\Ahttps?://}
+    require "tmpdir"
+    source_paths.unshift(tempdir = Dir.mktmpdir("groove-"))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      "--quiet",
+      "https://github.com/kyletress/groove.git",
+      tempdir
+    ].map(&:shellescape).join(" ")
+
+    if (branch = __FILE__[%r{groove/(.+)/template.rb}, 1])
+      Dir.chdir(tempdir) { git checkout: branch }
     end
-  end  
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
+end  
 
 def add_gems 
-    gem 'bcrypt', '~> 3.1.12'
-    gem 'name_of_person'
-    gem 'redis', '~> 4.0'
-    gem 'sidekiq', '~> 5.2', '>= 5.2.5'
+  gem 'bcrypt', '~> 3.1.12'
+  gem 'name_of_person'
+  gem 'redis', '~> 4.0'
+  gem 'sidekiq', '~> 5.2', '>= 5.2.5'
 end 
 
 def copy_templates
-    remove_file "app/assets/stylesheets/application.css"
-    directory "app", force: true
-    generate :controller, "StaticPages home"
+  remove_file "app/assets/stylesheets/application.css"
+  directory "app", force: true
+  generate :controller, "StaticPages home"
 
-    route "root to: 'static_pages#home'"
-    route "get  '/signup', to: 'users#new'"
-    route "post '/signup', to: 'users#create'"
-    route "resources :users"
-
+  route "root to: 'static_pages#home'"
+  route "get  '/signup', to: 'users#new'"
+  route "post '/signup', to: 'users#create'"
+  route "resources :users"
 end 
 
 def add_users 
-    generate :controller, "Users"
-    generate :model, "User first_name last_name email:string:uniq password_digest"
+  generate :controller, "Users"
+  generate :model, "User first_name last_name email:string:uniq password_digest"
+end 
+
+def configure_ssl
+  environment 'config.force_ssl = true', env: 'production'
 end 
 
 # Main setup
 add_template_repository_to_source_path
-
 add_gems
 add_users
+configure_ssl
 
 after_bundle do
-    copy_templates
-    
-    # Create and migrate the database
-    rails_command "db:create"
-    rails_command "db:migrate"
+  copy_templates
+  
+  # Create and migrate the database
+  rails_command "db:create"
+  rails_command "db:migrate"
 
-    # Initialize a git repo and commit everything 
-    git :init 
-    git add: "."
-    git commit: %Q{ -m 'Initial commit' }
+  # Initialize a git repo and commit everything 
+  git :init 
+  git add: "."
+  git commit: %Q{ -m 'Initial commit' }
 
-    say "Track is clear. Your app has been created.", :green
+  say "Track is clear. Your app has been created.", :green
 end
